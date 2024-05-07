810af9956ec254ae126ecd530bba8c5974d73c97
mereditsommer <meredit.sommer@youengineering.ch>
2024-05-07 12:18:03 +0200

    add pixel filter

--- src/main/java/meridia/utils/Filters.java
+++ src/main/java/meridia/utils/Filters.java
@@ -17,11 +17,11 @@ public class Filters {
                 int g = color.getGreen();
                 int b = color.getBlue();
                 // lightness is the average of the largest and smallest color components
-                Color newColor;
                 int max = Math.max(r, Math.max(g, b));
                 int min = Math.min(r, Math.min(g, b));
                 // lightness is the average of the largest and smallest color components
                 int lum = (max + min) / 2;
+                Color newColor;
                 if (lum > 127) {
                     newColor = Color.WHITE;
                 } else {
@@ -53,8 +53,21 @@ public class Filters {
     }
 
     public static void setPixelFilter(File file) throws IOException {
+        int pixelSize = 10;
         BufferedImage img = ImageIO.read(file);
+        int width = img.getWidth();
+        int height = img.getHeight();
 
+        for (int y = 0; y < height; y += pixelSize) {
+            for (int x = 0; x < width; x += pixelSize) {
+                Color pixelColor = getAverageColor(img, x, y, pixelSize);
+                for(int yd = y; (yd < y + pixelSize) && (yd < height); yd++) {
+                    for(int xd = x; (xd < x + pixelSize) && (xd < width); xd++) {
+                        img.setRGB(xd, yd, pixelColor.getRGB());
+                    }
+                }
+            }
+        }
         writeFile(img, file);
     }
 
@@ -72,5 +85,26 @@ public class Filters {
         String filePath = resourcePath + "meridia." + format;
         File target = new File(filePath);
         ImageIO.write(img, format, target);
+    }
+
+    private static Color getAverageColor(BufferedImage image, int startX, int startY, int size) {
+        int totalRed = 0, totalGreen = 0, totalBlue = 0;
+        int pixelCount = 0;
+
+        for (int y = startY; y < startY + size && y < image.getHeight(); y++) {
+            for (int x = startX; x < startX + size && x < image.getWidth(); x++) {
+                int rgb = image.getRGB(x, y);
+                totalRed += (rgb >> 16) & 0xFF;
+                totalGreen += (rgb >> 8) & 0xFF;
+                totalBlue += rgb & 0xFF;
+                pixelCount++;
+            }
+        }
+
+        int avgRed = totalRed / pixelCount;
+        int avgGreen = totalGreen / pixelCount;
+        int avgBlue = totalBlue / pixelCount;
+
+        return new Color(avgRed, avgGreen, avgBlue);
     }
 }
